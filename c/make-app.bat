5470b0507ede52cf66e917ac3fb4a4e29c5df319
gastonfeng <gastonfeng@gmail.com>
2017-05-15 17:10:38 +0800

    set path compiler

--- c/make-app.bat
+++ c/make-app.bat
@@ -1,3 +1,4 @@
+set path=%path%;C:\Program Files\IAR Systems\Embedded Workbench 7.0\stm8\bin\
 @echo off
 iccstm8 ./app/test.c -e -Oh --code_model  small --data_model medium --vregs 16 -o ./app/output/test.o
 ilinkstm8 --config_def _CSTACK_SIZE=0x100 --config_def _HEAP_SIZE=0x100 --config ./app/lnkstm8l101k3.icf ./app/output/test.o -o ./app/output/test.out
